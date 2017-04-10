3abe2d42b7eb2aff791e899ebfe5ea7dc6fa1075
Melissa <melissabrennan645@gmail.com>
2017-04-10 14:59:37 +0100

    Introduction to Java Programming / 10th Edition

---
+++ ShowLogicErrors.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Melissa on 10/04/2017.
+ * Chapter 1, Page 21 - Introduction to Computers, Programs and Java
+ */
+public class ShowLogicErrors {
+    public static void main(String[] args) {
+        System.out.println("Celsius 35 is Fahrenheit degree ");
+        System.out.println((9 / 5) * 35 + 32);
+    }
+}
---
+++ ShowRuntimeErrors.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Melissa on 10/04/2017.
+ * Chapter 1, Page 21 - Introduction to Computers, Programs and Java
+ */
+public class ShowRuntimeErrors
+{
+    public static void main(String[] args) {
+        System.out.println(1 / 0);
+    }
+}
