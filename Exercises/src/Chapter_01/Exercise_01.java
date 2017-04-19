9a80179d9ebd92d094429b554ac071c171189698
Melissa B <melissabrennan645@gmail.com>
2017-04-19 19:47:21 +0100

    Introduction to Java Programming / 10th Edition

---
+++ Exercises/src/Chapter_01/Exercise_01.java
@@ -0,0 +1,21 @@
+package Chapter_01;
+
+/**
+ * Introduction to Java Programming 10th Edition by Y Daniel Liang
+ * Chapter 1 Exercise 1:
+ *      (Display three messages) Write a program that displays
+ *      Welcome to Java, Welcome to Computer Science, and Programming is fun.
+ *
+ * Created by Melissa on 19/04/2017.
+ */
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+
+    }
+}
